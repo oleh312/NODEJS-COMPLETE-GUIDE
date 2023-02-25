b614a4aaaccd7a646c413692fc9d4e1307f5f8ca
oleh312 <oleh19127@gmail.com>
2023-02-25 16:18:29 +0200

    end 3.7 lesson

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const { log } = require("console");
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  log(req.url, req.method, req.headers);
+  //
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My first page</title><head>");
+  res.write("<body><h1>Hello from my nodejs app!!!</h1><body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
