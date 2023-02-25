9f4a76ff2f3cafa2b9e0f84523b1c0ed6b78b674
oleh312 <oleh19127@gmail.com>
2023-02-25 16:29:11 +0200

    complete 3.9 lesson

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const { log } = require("console");
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  //
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title><head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type ="text" name="message"><button type="submit">Send</button></form><body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My first page</title><head>");
