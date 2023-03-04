0c701c983f57ab113f8826f0ff6ccd31a2bd8ed8
oleh312 <oleh19127@gmail.com>
2023-03-04 18:56:50 +0200

    complete  lessons: 3.10, 3.11,
    3.12, 3.13, 3.14, 3.15, 4.1, 4.2, 4,3

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const { log } = require("console");
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
+
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter Message</title><head>");
@@ -11,6 +14,22 @@ const server = http.createServer((req, res) => {
     );
     res.write("</html>");
     return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      const formatMessage = message.split("+").join(" ");
+      fs.writeFileSync("message.txt", formatMessage);
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    });
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
