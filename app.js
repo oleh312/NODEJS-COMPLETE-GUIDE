a7430a0de27532ad0054bd07c038af1b859d09f0
oleh312 <oleh19127@gmail.com>
2023-03-10 14:20:23 +0200

    5.10

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/add-product", (req, res, next) => {
   res.send(
@@ -11,7 +11,7 @@ app.use("/add-product", (req, res, next) => {
   );
 });
 
-app.use("/product", (req, res, next) => {
+app.post("/product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
