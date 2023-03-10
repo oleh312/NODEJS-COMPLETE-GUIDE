6dbb56b3e531669643cc2a7e71528a46cf3fd6c9
oleh312 <oleh19127@gmail.com>
2023-03-10 13:34:34 +0200

    5.6

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
@@ -8,10 +6,10 @@ app.use((req, res, next) => {
   console.log("In the middleware");
   next();
 });
+
 app.use((req, res, next) => {
   console.log("In another middleware");
-  next();
+  res.send("<h1>Hello Form Express!!!</h1>");
 });
-const server = http.createServer(app);
 
-server.listen(3000);
+app.listen(3000);
